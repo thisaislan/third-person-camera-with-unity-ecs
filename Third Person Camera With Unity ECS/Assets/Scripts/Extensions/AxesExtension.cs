865c1be6b7e30534488a101b979e638e12f9b63b
aislan <this.aislan@gmail.com>
2020-03-14 10:55:17 -0300

    :sparkles: Add base project
    Base project: https://github.com/ThisAislan/third-person-camera

---
+++ Third Person Camera With Unity ECS/Assets/Scripts/Extensions/AxesExtension.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Axes = Metadatas.Input.Axes;
+
+namespace Extensions
+{
+
+    public static class AxesExtension
+    {
+        public static float GetAxis(this Axes axes) => Input.GetAxis(axes.ToString());
+
+        public static float GetAxisRaw(this Axes axes) => Input.GetAxisRaw(axes.ToString());
+
+    }
+
+}
